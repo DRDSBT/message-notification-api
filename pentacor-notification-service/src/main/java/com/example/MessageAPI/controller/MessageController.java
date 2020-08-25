3ff8fe82e0f64dd9e19f29cf8f7b99656fa58920
edrdsbt <eduard.sabot@yahoo.de>
2020-08-25 13:37:13 +0200

    Kommentar

--- pentacor-notification-service/src/main/java/com/example/MessageAPI/controller/MessageController.java
+++ pentacor-notification-service/src/main/java/com/example/MessageAPI/controller/MessageController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController // Kennung REST-API
 @RequestMapping("/api")
 public class MessageController {
 
